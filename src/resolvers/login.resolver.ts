38e0946a7c79c99144337b8ab1153ce096326680
guoyuan <guoyuan@icarbonx.com>
2018-10-09 09:21:22 +0800

    Add login mutation

--- src/resolvers/login.resolver.ts
+++ src/resolvers/login.resolver.ts
@@ -1,14 +1,27 @@
+import { User } from "../entity/User";
+import translateError from "../utils/translateError";
+import { compare } from "bcryptjs";
+// import { compare } from "bcryptjs";
+
 // what should of login mutaion
 export default {
-    Mutaion: {
-        login(_: any, {email, password}: any) {
-            console.log(email, password);
+    Mutation: {
+        async login(_: any, {email, password}: any) {
             // what should we do to handle login mutation?
             // Maybe: 
             // 0. get the query argments
             // 1. check the password with database hashed pasword
             // 2. if success, then return login status true
             // 3. fail, then throw some error!
+            const user = await User.findOne({email}).catch(translateError("user do not exists!"));
+            if (!user) throw new Error("user do not exists!");
+            const isExits = await compare(password, user.password);
+            if (isExits) {
+                return true;
+            } else {
+                throw new Error("Password error!");
+            }
+            // maybe need manage the login status like session or something
         }
     }
 }
